0967925863e8ef207ae7565692a4af1f70eba5c5
Veer Tkd <veer65patel@gmail.com>
2026-04-01 17:05:42 +0530

    first commit

--- hotels/admin.py
+++ hotels/admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
 from .models import Hotel, RoomType, HotelImage
 
-admin.site.register(Hotel)
+
+class HotelImageInline(admin.TabularInline):
+    model = HotelImage
+    extra = 3   # number of empty image fields
+
+
+@admin.register(Hotel)
+class HotelAdmin(admin.ModelAdmin):
+    inlines = [HotelImageInline]
+
+
 admin.site.register(RoomType)
-admin.site.register(HotelImage)
